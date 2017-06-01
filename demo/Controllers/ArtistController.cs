ba99cffe57d1eab3f001b50e7d1c76571ddba06e
BernardCooley <bernardcooley@gmail.com>
2017-06-01 21:58:02 +0100

    single api requests. Lineup from string array not api

--- demo/Controllers/ArtistController.cs
+++ demo/Controllers/ArtistController.cs
@@ -1,5 +1,4 @@
 ﻿using demo.Models;
-using System.Linq;
 using System.Collections.Generic;
 using System.Web.Mvc;
 using System.Xml;
@@ -7,51 +6,55 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System;
 using System.Threading.Tasks;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace demo.Controllers
 {
     public class ArtistController : Controller
     {
-        XmlDocument xd = new XmlDocument();
-        String xmlString = "";
-        StringBuilder sb = new StringBuilder();
-        List<ArtistDetails> artists = new List<ArtistDetails>();
-
-        async public Task<ActionResult> Index()
+        public ActionResult Index()
         {
-            await RunRequests(lineup(), xmlString);
-            xmlString = parseXMLString(sb);
-            xd.LoadXml(xmlString);
-            artists = LoadArtistsFromXml(xd);
-            var summaries = new List<ArtistSummary>();
-
-            artists.ForEach(a => summaries.Add(new ArtistSummary { ID = a.djid, Name = a.name }));
-            return View(summaries);
+            return View(lineup());
         }
 
         [HttpGet]
-        async public Task<ActionResult> GetArtist(int id)
+        async public Task<ActionResult> GetArtist(string artistName)
         {
-            await RunRequests(lineup(), xmlString);
-            xmlString = parseXMLString(sb);
-            xd.LoadXml(xmlString);
-            System.Diagnostics.Debug.WriteLine(xd);
-            artists = LoadArtistsFromXml(xd);
-            System.Diagnostics.Debug.WriteLine(artists.Count);
-            System.Diagnostics.Debug.WriteLine(id);
-            var selectedArtist = artists.FirstOrDefault(a => a.djid == id);
-            return Json(selectedArtist, JsonRequestBehavior.AllowGet);
+            XmlDocument xd = new XmlDocument();
+            xd.LoadXml(await GetArtistDetailsByName(artistName));
+            var artistDetails = LoadArtistsFromXml(xd);
+            return Json(artistDetails, JsonRequestBehavior.AllowGet);
         }
 
-        private List<Models.ArtistDetails> LoadArtistsFromXml(XmlDocument xd)
+        private ArtistDetails LoadArtistsFromXml(XmlDocument xd)
         {
-            System.Diagnostics.Debug.WriteLine("load artists from xml called");
-            var artists = new List<Models.ArtistDetails>();
+            var artistDetails = new ArtistDetails();
+
             foreach (XmlNode node in xd.SelectNodes("RA/artist"))
             {
-                artists.Add(new Models.ArtistDetails
+                artistDetails = new ArtistDetails
+                {
+                    djid = int.Parse(node["djid"].InnerText),
+                    name = node["artistname"].InnerText,
+                    country = node["countryname"].InnerText,
+                    labels = node["labels"].InnerText,
+                    website = node["website"].InnerText,
+                    raProfile = node["raprofile"].InnerText,
+                    twitter = node["twitter"].InnerText,
+                    facebook = node["facebook"].InnerText,
+                    discogs = node["discogs"].InnerText,
+                    profileImageURL = node["profileimage"].InnerText,
+                    bioSmall = node["biointro"].InnerText,
+                };
+            }
+            return artistDetails;
+        }
+
+        private List<ArtistDetails> LoadAllArtistsFromXml(XmlDocument xd)
+        {
+            var artists = new List<ArtistDetails>();
+            foreach (XmlNode node in xd.SelectNodes("RA/artist"))
+            {
+                artists.Add(new ArtistDetails
                 {
                     djid = int.Parse(node["djid"].InnerText),
                     name = node["artistname"].InnerText,
@@ -96,17 +99,16 @@ namespace demo.Controllers
             return lineup;
         }
 
-        async public Task RunRequests(List<string> lineup, string xmlString)
+        async public Task<String> GetArtistDetailsByName(string artistName)
         {
-            foreach(string s in lineup)
-            {
-                await PostRequest("https://www.residentadvisor.net/api/dj.asmx/getartist", "2090605", "a465bde8-ba57-4ce0-95af-923cb856ab9d", "", s, "", xd, sb);
-            }
+            return await PostRequest("https://www.residentadvisor.net/api/dj.asmx/getartist", "2090605", 
+                "a465bde8-ba57-4ce0-95af-923cb856ab9d", "", artistName, "");
         }
 
-        async static Task PostRequest(string url, string userID, string accessKey, string djID, string artistName, string artistURL, XmlDocument xd, StringBuilder sb)
+        async static Task<String> PostRequest(string url, string userID, string accessKey, string djID, 
+            string artistName, string artistURL)
         {
-            IEnumerable<KeyValuePair<string, string>> queries = new List<KeyValuePair<string, string>>()
+            IEnumerable <KeyValuePair<string, string>> queries = new List<KeyValuePair<string, string>>()
             {
                 new KeyValuePair<string, string>("UserID", userID),
                 new KeyValuePair<string, string>("AccessKey", accessKey),
@@ -122,28 +124,13 @@ namespace demo.Controllers
                 {
                     using (HttpContent content = response.Content)
                     {
-                        string myContent = await content.ReadAsStringAsync();
                         HttpContentHeaders headers = content.Headers;
-
-                        sb.Append(myContent);
-                        //System.Diagnostics.Debug.WriteLine(myContent);
+                        return await content.ReadAsStringAsync();
+                        
                     }
 
                 }
             }
-        }
-
-        public String parseXMLString(StringBuilder sb)
-        {
-            String xmlString = sb.ToString();
-            xmlString = xmlString.Replace("</RA>", "");
-            xmlString = xmlString.Replace("<RA>", "");
-            xmlString = xmlString.Replace("<?xml version=\"1.0\" encoding=\"utf-8\"?>", "");
-            xmlString = xmlString.Insert(0, "\n<RA>");
-            xmlString = xmlString.Insert(0, "<?xml version=\"1.0\" encoding=\"utf-8\"?>");
-            xmlString = xmlString.Insert(xmlString.Length, "</RA>");
-
-            return xmlString;
         }
     }
 }
